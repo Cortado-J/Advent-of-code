0fd948d791992e62d793700a0f652e14b5dad599
Adahus <42062906+adahus@users.noreply.github.com>
2022-12-10 17:41:38 +0000

    Day 1

--- AOC2022/Python/day01.py
+++ AOC2022/Python/day01.py
@@ -1,1 +1,14 @@
-# DAY 1 Not saved!
+day = 1
+sections = open(f"input{day:02d}.txt").read().split('\n\n')
+elves = []
+for section in sections:
+    count = 0
+    for calories in section.splitlines():
+        count += int(calories)
+    elves.append(count)
+elves.sort(reverse=1)
+
+parta = elves[0]
+print(f"Day {day}: Part A = {parta}")
+partb = sum(elves[0:3])
+print(f"Day {day}: Part B = {partb}")
