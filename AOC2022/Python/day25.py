b8b91861de006851ff9387d4a928dd191d36e9e9
Adahus <42062906+adahus@users.noreply.github.com>
2022-12-25 06:34:23 +0000

    Day 25

---
+++ AOC2022/Python/day25.py
@@ -0,0 +1,33 @@
+import os
+
+day = int(os.path.basename(__file__)[-5:-3])
+filename = f"input{day:02d}.txt"
+lines = open(filename).read().splitlines()
+
+
+def dig(x): return '=-012'.index(x) - 2
+
+
+def getdigit(n, pos):
+    startfactor = sum([5 ** i for i in range(pos + 1)])
+    return "=-012"[((n + 2 * startfactor) // (5 ** pos)) % 5]
+
+
+def snafu(n):
+    result = ''
+    for pos in range(40):  # Max 40 digits
+        result = str(getdigit(n, pos)) + result
+    while len(result) > 0 and result[0] == '0':
+        result = result[1:]  # Strip leading zeroes
+    return result
+
+
+def dec(s):
+    acc = 0
+    for d in s:
+        acc = acc * 5 + dig(d)
+    return acc
+
+
+parta = snafu(sum([dec(s) for s in lines]))
+print(f"Day {day}: Part A = {parta}")
